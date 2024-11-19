d963c722eb5f061870ef8d11ba912847f8a86c85
kewalin ngamchan <kewalin.ngamchan@gmail.com>
2024-11-19 20:50:01 +0100

    Updated version 2.0

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.layout = html.Div([
             {'label': 'Ski Jumping', 'value': 'Ski Jumping'},
             {'label': 'Rowing', 'value': 'Rowing'}
         ],
-        value='Age'
+        value='Select Feature'
     ),
     dcc.Graph(id='histogram')
 ])
@@ -35,8 +35,9 @@ app.layout = html.Div([
 )
 
 def update_histogram(selected_feature):
+# bara australien
     if selected_feature == 'medals_per_os':
-        medals_per_os = aus_df.groupby('Medal')['Sport'].count().resest_index()
+        medals_per_os = aus_df.groupby('Medal')['Sport'].count().reset_index()
         fig = px.histogram(medals_per_os, x='Medal', y='Sport', histfunc='sum',
                            title='Medals per Sport')
         
@@ -54,27 +55,29 @@ def update_histogram(selected_feature):
         top_10_sports = sorted_medals.head(10)
         fig = px.histogram(top_10_sports, x='Sport', y='Medal',
                      title='Top 10 Sports with Most Medals')
+
+# alla andra länder 
         
     elif selected_feature == 'Tug-Of-War':
-        tug_of_war_df = aus_df[aus_df['Sport'] == 'Tug-Of-War']
+        tug_of_war_df = df[df['Sport'] == 'Tug-Of-War']
         fig = px.histogram(tug_of_war_df, x='Year', title='Tug-Of-War Participation Over Time')
         fig.update_layout(xaxis_title='Year', yaxis_title='Frequency')
 
     elif selected_feature == 'Basketball':
-        basketball_df = aus_df[aus_df['Sport'] == 'Basketball']
+        basketball_df = df[df['Sport'] == 'Basketball']
         fig = px.histogram(basketball_df, x='Year', title='Basketball Participation Over Time')
         fig.update_layout(xaxis_title='Year', yaxis_title='Frequency')
 
     elif selected_feature == 'Ski Jumping':
-        ski_jumping_df = aus_df[aus_df['Sport'] == 'Ski Jumping']
+        ski_jumping_df = df[df['Sport'] == 'Ski Jumping']
         fig = px.histogram(ski_jumping_df, x='Year', title='Ski Jumping Participation Over Time')
         fig.update_layout(xaxis_title='Year', yaxis_title='Frequency')
-
-    elif selected_feature == 'Rowing':
-        rowing_df = aus_df[aus_df['Sport'] == 'Rowing']
+    
+    else: 
+        rowing_df = df[df['Sport'] == 'Rowing']
         fig = px.histogram(rowing_df, x='Year', title='Rowing Participation Over Time')
         fig.update_layout(xaxis_title='Year', yaxis_title='Frequency')
-
+    
     return fig
 
 if __name__ == '__main__':
